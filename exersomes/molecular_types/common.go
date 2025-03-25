0b958c4465deb824808b1686354b5fada96ffea7
Daniel J. Gomez <sfdanielgomez@gmail.com>
2025-03-25 16:44:16 -0700

    Update common.go

--- exersomes/molecular_types/common.go
+++ exersomes/molecular_types/common.go
@@ -1,4 +1,28 @@
-package common
+// exersomes/molecular_types/common.go
+package molecular_types
+
+type Exerkine struct {
+    Name            string
+    Category        string  // Protein, Metabolite, miRNA, etc.
+    TissueSources   []string
+    BiologicalFunc  string
+    Sequence        string  // Chemical formula or AA sequence
+    PDBID          string  // For proteins
+    Receptors       []string
+}
+
+type ExerkineNetwork struct {
+    Nodes         []Exerkine
+    Interactions  []Interaction
+}
+
+type Interaction struct {
+    Ligand      string
+    Receptor    string
+    Tissue      string
+    Pathway     string
+    Strength    float64 // Optional confidence score
+}
 
 // ExerciseMolecule represents a common interface for all exercise-responsive molecules
 type ExerciseMolecule interface {
