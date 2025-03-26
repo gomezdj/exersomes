5f6971aa6bcb323d7f7d6020f82ea24dc9c44ae8
Daniel J. Gomez <sfdanielgomez@gmail.com>
2025-03-25 17:01:09 -0700

    Create data_analysis.go

---
+++ src/data_analysis.go
@@ -0,0 +1,22 @@
+// src/data_analysis.go
+package main
+
+import (
+    "github.com/djgomez8/exersomes/molecular_types"
+)
+
+func BuildNetwork(exerkines []molecular_types.Exerkine) molecular_types.ExerkineNetwork {
+    network := molecular_types.ExerkineNetwork{
+        Nodes: exerkines,
+    }
+    
+    // Implement interaction detection logic
+    // Could integrate with CellPhoneDB or other databases
+    
+    return network
+}
+
+func FindKeyRegulators(network molecular_types.ExerkineNetwork) []string {
+    // Implement network analysis to find hubs
+    return []string{}
+}
